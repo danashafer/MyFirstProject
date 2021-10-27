cb305815a1a9c04371a6343dea1df79fc5b1438c
Dana Shafer <shafer.dana@gmail.com>
2021-10-27 18:44:14 +0300

    .

---
+++ src/main/java/frc/robot/commands/ActivateFeeder.java
@@ -0,0 +1,43 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Feeder;
+
+public class ActivateFeeder extends CommandBase {
+  /** Creates a new ActivateFeeder. */
+  private final Feeder feeder;
+  private final double percent;
+  public ActivateFeeder(Feeder feeder,double percent) {
+    // Use addRequirements() here to declare subsystem dependencies.
+    this.feeder = feeder;
+    this.percent = percent;
+
+    addRequirements(feeder);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    this.feeder.setPercentOutput(this.percent);
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    this.feeder.setPercentOutput(this.percent);
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
---
+++ src/main/java/frc/robot/subsystems/Feeder.java
@@ -0,0 +1,33 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.Victor;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+public class Feeder extends SubsystemBase {
+
+  private final Victor master;
+  /** Creates a new Feeder. */
+  public Feeder() {
+    master = new Victor(Constants.feederID);
+    stop();
+  }
+
+  public void setPercentOutput(double percent){
+    master.set(percent);
+  }
+  public void stop(){
+    master.set(0);
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+
+}
